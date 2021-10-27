ce350d588f96aefe39dd66a960c801f2bc031642
password-kim <72890183+password-kim@users.noreply.github.com>
2021-10-27 16:42:13 +0900

    Make DepthStencilBuffer.

--- Game/Engine/Engine.cpp
+++ Game/Engine/Engine.cpp
@@ -7,7 +7,6 @@
 void Engine::Init(const WindowInfo& info)
 {
 	_window = info;
-	ResizeWindow(info.width, info.height);
 
 	// 그려질 화면 크기를 설정
 	_viewport = { 0, 0, static_cast<FLOAT>(info.width), static_cast<FLOAT>(info.height), 0.0f, 1.0f };
@@ -19,6 +18,7 @@ void Engine::Init(const WindowInfo& info)
 	_rootSignature = make_shared<RootSignature>();
 	_cb = make_shared<ConstantBuffer>();
 	_tableDescHeap = make_shared<TableDescriptorHeap>();
+	_depthStencilBuffer = make_shared<DepthStencilBuffer>();
 
 	_device->Init();
 	_cmdQueue->Init(_device->GetDevice(), _swapChain);
@@ -26,6 +26,9 @@ void Engine::Init(const WindowInfo& info)
 	_rootSignature->Init();
 	_cb->Init(sizeof(Transform), 256);
 	_tableDescHeap->Init(256);
+	_depthStencilBuffer->Init(_window);
+
+	ResizeWindow(info.width, info.height);
 }
 
 
@@ -56,4 +59,6 @@ void Engine::ResizeWindow(int32 width, int32 height)
 	RECT rect = { 0, 0, width, height };
 	::AdjustWindowRect(&rect, WS_OVERLAPPEDWINDOW, false);
 	::SetWindowPos(_window.hwnd, 0, 100, 100, width, height, 0);
+
+	_depthStencilBuffer->Init(_window);
 }
