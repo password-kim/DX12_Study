cdcb4fdb9c1d9f1a0b05702290809ad4cde1644c
password-kim <72890183+password-kim@users.noreply.github.com>
2021-10-30 11:56:00 +0900

    Implement component.

--- Game/Engine/Engine.cpp
+++ Game/Engine/Engine.cpp
@@ -1,9 +1,7 @@
 #include "pch.h"
 #include "Engine.h"
-#include "Device.h"
-#include "CommandQueue.h"
-#include "SwapChain.h"
 #include "Material.h"
+#include "Transform.h"
 
 void Engine::Init(const WindowInfo& info)
 {
@@ -23,7 +21,7 @@ void Engine::Init(const WindowInfo& info)
 	_input->Init(info.hwnd);
 	_timer->Init();
 
-	CreateConstantBuffer(CBV_REGISTER::b0, sizeof(Transform), 256);
+	CreateConstantBuffer(CBV_REGISTER::b0, sizeof(TransformMatrix), 256);
 	CreateConstantBuffer(CBV_REGISTER::b1, sizeof(MaterialParams), 256);
 
 	ResizeWindow(info.width, info.height);
@@ -45,6 +43,11 @@ void Engine::Update()
 	_timer->Update();
 
 	ShowFps();
+}
+
+void Engine::LateUpdate()
+{
+	// TODO
 }
 
 void Engine::RenderBegin()
---
+++ Game/Engine/Transform.cpp
@@ -0,0 +1,12 @@
+#include "pch.h"
+#include "Transform.h"
+
+Transform::Transform() : Component(COMPONENT_TYPE::TRANSFORM)
+{
+
+}
+
+Transform::~Transform()
+{
+
+}
